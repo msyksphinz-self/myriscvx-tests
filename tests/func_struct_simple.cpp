e8888868f5d8dbf1e32798e65eef67de7cc069b3
msyksphinz <msyksphinz_dev.gmail.com>
2019-07-09 16:00:16 +0900

    Func: update func_struct_simple.cpp

--- tests/func_struct_simple.cpp
+++ tests/func_struct_simple.cpp
@@ -11,7 +11,10 @@ void func (struct S elem) {
   }
 }
 
-// void call_func () {
-//   struct_type elem = {10, 20};
-//   func (elem);
-// }
+void call_func () {
+  struct S elem;
+  for (int i = 0; i < 17; i++) {
+    elem.x[i] = i;
+  }
+  func (elem);
+}
