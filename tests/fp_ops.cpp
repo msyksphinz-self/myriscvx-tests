4f4265c958b9aa65ff14c2eb5b95ebd3a0daa460
msyksphinz <msyksphinz_dev@gmail.com>
2019-08-02 11:28:54 +0900

    Func: add float/double ops tests

---
+++ tests/fp_ops.cpp
@@ -0,0 +1,15 @@
+void test_fp_math(float *a, float *b, float *c)
+{
+  float res_add = *a + *b + *c;
+  float res_sub = *a - *b - *c;
+  float res_mul = *a * *b * *c;
+  float res_div = *a / *b / *c;
+}
+
+void test_dp_math(double *a, double *b, double *c)
+{
+  double res_add = *a + *b + *c;
+  double res_sub = *a - *b - *c;
+  double res_mul = *a * *b * *c;
+  double res_div = *a / *b / *c;
+}
