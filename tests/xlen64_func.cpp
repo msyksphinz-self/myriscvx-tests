de229e4afd6111ab3492864f4bc461e3835fd575
msyksphinz <msyksphinz_dev@gmail.com>
2019-08-13 22:54:18 +0900

    Func: update xlen64_func.cpp

--- tests/xlen64_func.cpp
+++ tests/xlen64_func.cpp
@@ -2,10 +2,11 @@
 
 int64_t func(int64_t a0, int64_t a1)
 {
-  return a0 + a1;
+  return a0 - a1;
 }
 
 
 int main() {
-  return func(10, 20);
+  volatile uint64_t result;
+  result = func(10, 20);
 }
